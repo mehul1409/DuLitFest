5491f49302d8c8b4cd13a064b3e909309bb2c8f7
Harsh Sharma <harshsharma20503@gmail.com>
2024-01-07 21:22:26 +0530

    registeration page in progress

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,13 @@
 import Footer from "../../Components/Footer/Footer";
 import PageHeader from "../../Components/PageHeader/PageHeader";
+import RegisterForm from "../../Components/RegisterForm/RegisterForm";
 import "./Register.scss";
 
 const Register = () => {
   return (
     <>
       <PageHeader heading="Register" />
+      <RegisterForm />
       <Footer />
     </>
   );
