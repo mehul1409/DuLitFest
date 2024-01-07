204c002b13a7e033acecfbbe4db924b79096763b
Yash Mittal <mittalyas1234@gmail.com>
2024-01-07 20:14:42 +0530

    event page done

--- src/Pages/Programme/Programme.jsx
+++ src/Pages/Programme/Programme.jsx
@@ -2,6 +2,7 @@ import PageHeader from "../../Components/PageHeader/PageHeader";
 import EventDetailsDay1 from "../../assets/data/EventDetailsDay1";
 import EventDetailsDay2 from "../../assets/data/EventDetailsDay2";
 import EventDetailsDay3 from "../../assets/data/EventDetailsDay3";
+import { motion } from 'framer-motion'
 import "./Programme.scss";
 import Footer from "../../Components/Footer/Footer";
 
@@ -10,58 +11,120 @@ const Programme = () => {
     <>
       <PageHeader heading="Programme" bgColor="" />
       <div className="outer-1">
-        <div className="box-1">
-          <div className="Day1">
+        <motion.div
+          initial={{ x: '-100vw' }}
+          animate={{ x: '0.7vw' }}
+          transition={{ duration: 2 }}
+          className="box-1">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            viewport={{ once: true }}
+            className="Day1">
             Day-1 -19th January,2024
-          </div>
+          </motion.div>
           <ul>
             {EventDetailsDay1.map((event, index) => (
-              <div className="one" key={index}>
+              < motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 5, delay: 0.1 }}
+                viewport={{ once: true }}
+                className="one" key={index}
+                style={{
+                  backgroundColor:
+                    event.Place === 'Shridhar Shriram Auditorium'
+                      ? 'pink'
+                      : 'yellow',
+                }}>
                 <div className="top">
                   <p>{event.time}</p>
                   <p>{event.Place}</p>
                 </div>
                 <h1> {event.Event_Name}</h1>
                 <p> {event.Address}</p>
-              </div>
+              </motion.div>
             ))}
           </ul>
-        </div>
-        <div className="box-1">
-          <div className="Day2">
+        </motion.div>
+
+        <motion.div
+          initial={{ x: '-100vw' }}
+          animate={{ x: '0.7vw' }}
+          transition={{ duration: 2 }}
+          className="box-2">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            viewport={{ once: true }}
+            className="Day1">
             Day-2 -20th January,2024
-          </div>
+          </motion.div>
           <ul>
             {EventDetailsDay2.map((event, index) => (
-              <div className="one" key={index}>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 5, delay: 0.1 }}
+                viewport={{ once: true }}
+                className="one" key={index}
+                style={{
+                  backgroundColor:
+                    event.Place === 'Shridhar Shriram Auditorium'
+                      ? 'pink'
+                      : 'yellow',
+                }}>
                 <div className="top">
                   <p>{event.time}</p>
                   <p>{event.Place}</p>
                 </div>
                 <h1> {event.Event_Name}</h1>
                 <p> {event.Address}</p>
-              </div>
+              </motion.div>
             ))}
           </ul>
-        </div>
-        <div className="box-1">
-          <div className="Day3">
+        </motion.div>
+
+
+        <motion.div
+          initial={{ x: '-100vw' }}
+          animate={{ x: '0.7vw' }}
+          transition={{ duration: 2 }} className="box-1">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            viewport={{ once: true }}
+            className="Day1">
             Day-3 -20th January,2024
-          </div>
+          </motion.div>
           <ul>
             {EventDetailsDay3.map((event, index) => (
-              <div className="one" key={index}>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 5, delay: 0.1 }}
+                viewport={{ once: true }}
+                className="one" key={index}
+                style={{
+                  backgroundColor:
+                    event.Place === 'Shridhar Shriram Auditorium'
+                      ? 'pink'
+                      : 'yellow',
+                }}>
                 <div className="top">
                   <p>{event.time}</p>
                   <p>{event.Place}</p>
                 </div>
                 <h1> {event.Event_Name}</h1>
                 <p> {event.Address}</p>
-              </div>
+              </motion.div>
             ))}
           </ul>
-        </div>
-      </div>
+        </motion.div>
+      </div >
       <Footer />
     </>
   );
