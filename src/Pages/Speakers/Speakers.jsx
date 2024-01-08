1647b2c3431efededeca6afe4dc52f626628e392
Yash Mittal <mittalyas1234@gmail.com>
2024-01-09 03:56:28 +0530

    speaker page changes done

--- src/Pages/Speakers/Speakers.jsx
+++ src/Pages/Speakers/Speakers.jsx
@@ -13,19 +13,7 @@ const Speakers = () => {
         heading="Speakers"
         textColor="white"
       />
-      {/* <div className="flex items-center justify-center min-h-screen container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {SpeakersDetails.map((speaker, index) => (
-            <SpeakerCard
-              key={index}
-              imgUrl={speaker.imgUrl}
-              name={speaker.name}
-              tag={speaker.tag}
-            />
-          ))}
-        </div>
-      </div> */}
-      <div className="team_container">
+      <div className="team_container ">
         <div className="card_container">
           {SpeakersDetails.map((speaker, index) => (
             <SpeakerCard
@@ -37,6 +25,18 @@ const Speakers = () => {
           ))}
         </div>
       </div>
+      {/* <div className="team_container">
+        <div className="card_container">
+          {SpeakersDetails.map((speaker, index) => (
+            <SpeakerCard
+              key={index}
+              imgUrl={speaker.imgUrl}
+              name={speaker.name}
+              tag={speaker.tag}
+            />
+          ))}
+        </div>
+      </div> */}
       <Footer bgColor="#aa4b6b" />
     </>
   );
