1172542321da40aa5135419933d057f711657585
Harsh Sharma <harshsharma20503@gmail.com>
2024-01-10 12:52:56 +0530

    explore carousel added

--- src/Components/Crousel/Crousel.jsx
+++ src/Components/Crousel/Crousel.jsx
@@ -1,25 +1,26 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
-import spons from '../ExploreCard/spons.png'
-import contact from '../ExploreCard/contact.png'
-import speak from '../ExploreCard/speak.png'
-import time from '../ExploreCard/time.png'
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import spons from "/assets/images/explorePartners1.png";
+import register from "/assets/images/exploreRegister1.png";
+import speak from "/assets/images/exploreSpeaker1.png";
+import time from "/assets/images/exploreProgramme1.png";
 
-import './Crousel.css';
+import "./Crousel.css";
 
 // import required modules
-import { FreeMode, Pagination } from 'swiper/modules';
+import { FreeMode, Pagination } from "swiper/modules";
 
 export default function App() {
   return (
     <>
-         <Swiper className="swip-out" 
+      <Swiper
+        className="swip-out"
         slidesPerView={3}
         spaceBetween={30}
         loop={true}
@@ -27,34 +28,31 @@ export default function App() {
         pagination={{
           clickable: true,
         }}
-        modules={[FreeMode, Pagination]} >
-        <SwiperSlide className='swip-box'>
-            <img src={spons} alt="sponser" className='crousimg' />
-            <div className="crouss-div">
-                <div className="crous-text1">Partners</div>
-                <div className="crous-text2">More</div>
-            </div>
+        modules={[FreeMode, Pagination]}
+      >
+        <SwiperSlide className="swip-box">
+          <img src={spons} alt="sponser" className="crousimg" />
+          <div className="crouss-div">
+            <div className="crous-text1">Partners</div>
+          </div>
         </SwiperSlide>
-        <SwiperSlide className='swip-box'>
-        <img src={contact} alt="sponser" className='crousimg' />
-        <div className="crouss-div">
-                <div className="crous-text1">Contact</div>
-                <div className="crous-text2">More</div>
-            </div>
+        <SwiperSlide className="swip-box">
+          <img src={register} alt="sponser" className="crousimg" />
+          <div className="crouss-div">
+            <div className="crous-text1">Register</div>
+          </div>
         </SwiperSlide>
-        <SwiperSlide className='swip-box'>
-        <img src={time} alt="sponser" className='crousimg' />
-        <div className="crouss-div">
-                <div className="crous-text1">Programme</div>
-                <div className="crous-text2">More</div>
-            </div>
+        <SwiperSlide className="swip-box">
+          <img src={time} alt="sponser" className="crousimg" />
+          <div className="crouss-div">
+            <div className="crous-text1">Programme</div>
+          </div>
         </SwiperSlide>
-        <SwiperSlide className='swip-box'>
-        <img src={speak} alt="sponser" className='crousimg' />
-        <div className="crouss-div">
-                <div className="crous-text1">Speakers</div>
-                <div className="crous-text2">More</div>
-            </div>
+        <SwiperSlide className="swip-box">
+          <img src={speak} alt="sponser" className="crousimg" />
+          <div className="crouss-div">
+            <div className="crous-text1">Speakers</div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </>
