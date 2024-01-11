3aa4359baa033e1daefa2a71b7d4e610dc163ce8
Rajatt09 <rajatbhati9559@gmail.com>
2024-01-11 23:12:15 +0530

    added dloaded

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from "./Loader.module.css"
+
+const Loader = () => {
+  return (
+    <>
+    <div className={styles.loader_body}>
+    {/* <div className={styles.spinner_container}> */}
+  
+{/* </div> */}
+{/* <div className='loadbox'> */}
+<img src="/assets/images/Logo.svg" alt='Please-wait' className={styles.loader_du_img}></img>
+{/* </div> */}
+   
+
+    </div>
+    </>
+  )
+}
+
+export default Loader
