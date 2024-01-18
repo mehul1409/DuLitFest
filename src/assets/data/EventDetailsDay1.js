f33237936cb9eb53d5a0777691c49bfeb9a23adb
Naman Gupta <namangupta990@gmail.com>
2024-01-18 15:48:58 +0530

    Updated day 1 events

--- src/assets/data/EventDetailsDay1.js
+++ src/assets/data/EventDetailsDay1.js
@@ -4,13 +4,19 @@ const EventDetailsDay1 = [
         "time": "11:00-11:50 AM",
         "Place": "Shridhar Shriram Auditorium",
         "Event_Name": "Inaugration Ceremony",
-        "Address": "",
+        "Address": "Festival Director Dr. Swapan Dasgupta, Dean of Colleges (DU) Prof. Balaram Pani & Principal SRCC Prof. Simrit Kaur",
     },
     {
         "time": "12:00-12:50 PM",
         "Place": "Shridhar Shriram Auditorium",
-        "Event_Name": "The Devil's Advocate & Sound and Fury ",
-        "Address": "Karan Thapar in conversation",
+        "Event_Name": "The Devil's Advocate & Sound & Fury ",
+        "Address": "Karan Thapar in conversation with Rishabh Shah",
+    },
+{
+        "time": "12:00-12:50 PM",
+        "Place": "Seminar Hall",
+        "Event_Name": "Sangam Talks:The Education System of India",
+        "Address": "Talk by Mr. Priyank Kanungo",
     },
     {
         "time": "1:00 - 1:50 PM",
@@ -18,60 +24,60 @@ const EventDetailsDay1 = [
         "Event_Name": "Being Hindu in Bangladesh: The Untold Story",
         "Address": "Deep Halder & Devdutta Majji in conversation with Utpal Kumar",
     },
-    {
-        "time": "1:00 - 1:50 PM",
-        "Place": "Front Lawn",
-        "Event_Name": "Forever Ayodhya",
-        "Address": "Koenraad Elst in conversation with Lipika Bhushan",
-    },
+    
+    
     {
         "time": "2:00 - 2:50 PM",
+        "Place": "Shridhar Shriram Auditorium",
+        "Event_Name": "Burning Roses in my Garden",
+        "Address": " Taslima Nasrin in conversation with Prof. Harish Trivedi",
+    },
+ {
+        "time": "3:00 - 3:50 PM",
+        "Place": "Shridhar Shriram Auditorium",
+        "Event_Name": "Nothing But the Truth",
+        "Address": "Rishabh Shah in conversation with Sahil Aggarwal",
+    },
+{
+        "time": "3:00 - 3:50 PM",
         "Place": "Front Lawn",
         "Event_Name": "Relationship Hacks in the Times of Gen-Alpha",
         "Address": "Mental Health Workshop with Smita Barooah",
     },
-    {
-        "time": "3:00 - 3:50 PM",
-        "Place": "Shridhar Shriram Auditorium",
-        "Event_Name": "Burning Roses in my Garden",
-        "Address": " Surprise guest in conversation with Harish Trivedi",
-    },
-    {
-        "time": "3:00 - 3:50 PM",
-        "Place": "Front Lawn",
-        "Event_Name": "Indian Attitude Towards Knowledge",
-        "Address": "Prof. Kapil Kapoor in conversation with Sampadananda Mishra",
-    },
-    {
-        "time": "4:00 - 4:50 PM",
-        "Place": "Shridhar Shriram Auditorium",
-        "Event_Name": "Bandi Jeevan : A Life in Chains",
-        "Address": "Sanjeev Sanyal in conversation with Chandrachur Ghosh",
-    },
-    {
-        "time": "4:00 - 4:50 PM",
-        "Place": "Front Lawn",
-        "Event_Name": "Nothing But the Truth",
-        "Address": "Rishabh Shah in conversation with Mohit Gupta",
-    },
-    {
-        "time": "5:00 PM - 5:50 PM",
+   
+ {
+        "time": "4:00 PM - 4:50 PM",
         "Place": "Shridhar Shriram Auditorium",
         "Event_Name": "The Art of Habits",
         "Address": "Keynote Address- Gauranga Das Prabhu",
 
     },
-    {
-        "time": "5:00 - 5:50 PM",
+ {
+        "time": "4:00 - 4:50 PM",
         "Place": "Front Lawn",
         "Event_Name": "She The Leader : Women in Politics",
         "Address": "Nidhi Sharma in conversation with Priya Sharma",
     },
+ {
+        "time": "5:00 - 5:50 PM",
+        "Place": "Shridhar Shriram Auditorium",
+        "Event_Name": "Bandi Jeevan : A Life in Chains",
+        "Address": "Sanjeev Sanyal in conversation with Chandrachur Ghosh",
+    },
+   
+   
+{
+        "time": "5:00 - 5:50PM",
+        "Place": "Front Lawn",
+        "Event_Name": "Forever Ayodhya",
+        "Address": "Koenraad Elst & Shridhar Potaraju in conversation with Lipika Bhushan",
+    },
+   
     {
         "time": "6:00 PM - 6:50 PM",
         "Place": "Shridhar Shriram Auditorium",
         "Event_Name": "Book Launch: Mahabharata Unravelled II : The Dharma Discourses",
-        "Address": "Ami Ganatra & J. Sai Deepak in conversation with Sanjeev Sanyal",
+        "Address": "Sanjeev Sanyal, Ami Ganatra & J. Sai Deepak in conversation with Anisha Kaul",
     },
 ]
 
